9be3eecbd9f59ea021318adbf5fb0ba0445543ca
batgr <80541212+batgr@users.noreply.github.com>
2024-12-30 13:03:13 +0100

    ajout de la checkbox dans le composant Task

--- src/main/kotlin/UI/Task.kt
+++ src/main/kotlin/UI/Task.kt
@@ -1,17 +1,42 @@
 package UI
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 @Composable
 private fun Task(modifier: Modifier = Modifier, title:String){
-    Card {
-        Text(title)
+    var checkState by rememberSaveable{ mutableStateOf(false) }
+
+    Card(
+        modifier=Modifier.padding(8.dp).size(width = 400.dp, height = 100.dp),
+        elevation = 2.dp
+        ) {
+        Row(modifier=modifier.padding(4.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(
+                checked = checkState,
+                onCheckedChange = {checkState=it},
+
+                )
+            Text(title, fontWeight = FontWeight.SemiBold)
+        }
+
     }
 }
 
